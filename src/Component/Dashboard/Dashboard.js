cdcdf7a55ad6647d0d0b7dfc30bb0baba71a53bf
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-04-09 00:34:05 +0600

    added rechart first charts

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
 import useData from '../Hooks/useData';
+import MyLineChart from '../MylineChart/MyLineChart';
 
 const Dashboard = () => {
     const [data,setData] = useData([])
     return (
         <div>
-            
+            <div className="mylineChart">
+                <LineChart width={400} height={400} data={data}>
+                    <Line dataKey='sell' stroke="#8884d8"></Line>
+                    <XAxis dataKey='month' />
+                    <YAxis />
+                </LineChart>
+            </div>
         </div>
     );
 };
