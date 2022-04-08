5931d2f578d459a53fead9ee370e187aa6d5cb6d
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-04-09 01:16:59 +0600

    Error page added final style

--- src/Component/ErrorPage/Error.js
+++ src/Component/ErrorPage/Error.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Error.css';
 
 const Error = () => {
     return (
         <div>
-            <h2>this is error page</h2>
+            <div className="error-main-area text-center">
+                <div className="error img text-center">
+                    <img src={"https://img.freepik.com/free-vector/404-error-with-person-looking-concept-illustration_114360-7912.jpg?size=626&ext=jpg&ga=GA1.1.930408535.1648299895"} alt="" />
+                </div>
+                <h2 className='error-text text-center'><span className='opp-cls'>Oops!</span> Page Not Found...</h2>
+                <div className="return-home text-center">
+                    <Button variant="success">
+                        <Link className='error-link' to='/home'>Back To Home</Link>
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
